47d6aed849bf3a602f1593dfab290f15cc7f4956
ST10448839 <161827877+ST10448839@users.noreply.github.com>
2025-09-08 06:33:11 +0200

    Created the WPF Application
    A WPF Application was created but only it being visible experience, there is no functionality yet because that will come later on in the POE

---
+++ ST10448839_POE_Part1/ST10448839_POE_Part1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ST10448839_POE_Part1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ST10448839_POE_Part1/ST10448839_POE_Part1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10448839_POE_Part1
+{
+    using System;
+    using System.Windows;
+
+    namespace CMCS
+    {
+        public partial class MainWindow : Window
+        {
+            public MainWindow()
+            {
+                InitializeComponent(); // This will now exist
+            }
+
+            private void InitializeComponent()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
